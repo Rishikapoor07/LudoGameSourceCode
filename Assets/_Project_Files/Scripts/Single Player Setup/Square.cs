fc6af43478dc926783e7b3745d2a2c0dc684793b
Rishikapoor07 <114287728+Rishikapoor07@users.noreply.github.com>
2022-09-28 17:37:23 +0530

    Changes in Waypoint scripts

--- Assets/_Project_Files/Scripts/Single Player Setup/Square.cs
+++ Assets/_Project_Files/Scripts/Single Player Setup/Square.cs
@@ -25,57 +25,11 @@ public class Square : MonoBehaviour
 	public void AddToken(Token token)
 	{
 		tokens.Add(token);
-		ResizeTokens();
 	}
 
 	public void RemoveToken(Token token)
 	{
 		tokens.Remove(token);
-		ResizeTokens();
-
 		token.transform.localScale = new Vector3(1f, 1f, 1f);
-	}
-
-	void ResizeTokens()
-	{
-		return;   /////Remove this later on
-		if (Tokens.Count > 1)
-		{
-			float scale = 1.0f - ((float)Tokens.Count / 6.0f);
-			float offset = scale / 5.0f;
-
-			List<Token> t = new List<Token>();
-			t.AddRange(Tokens);
-			if (Tokens.Count % 2 != 0)
-			{
-				Tokens[0].transform.localScale = new Vector3(scale, scale);
-				Tokens[0].transform.position = transform.position;
-				t.Remove(Tokens[0]);
-			}
-
-			int inc = 1;
-			for (int i = 0; i < t.Count; i++)
-			{
-				t[i].transform.localScale = new Vector3(scale, scale);
-
-				if (i % 3 == 0)
-				{
-					t[i].transform.position = transform.position + new Vector3(offset * inc, offset * inc);
-				}
-				else if (i % 3 == 1)
-				{
-					t[i].transform.position = transform.position + new Vector3(-offset * inc, -offset * inc);
-				}
-				else
-				{
-					inc++;
-				}
-			}
-		}
-		else if (Tokens.Count == 1)
-		{
-			Tokens[0].transform.localScale = new Vector3(1f, 1f);
-			Tokens[0].transform.position = transform.position;
-		}
 	}
 }
