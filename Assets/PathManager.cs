51505ab88b754d6ffe03312f7ce196d3d8c93b5b
Rishikapoor07 <114287728+Rishikapoor07@users.noreply.github.com>
2022-09-28 19:32:40 +0530

    Working on Scaling and positioning.

---
+++ Assets/PathManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathManager : MonoBehaviour
+{
+    public float[] scale;
+    public float[] positions;
+    
+}
